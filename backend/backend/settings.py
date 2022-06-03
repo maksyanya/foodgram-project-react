8bb4fe5fa598e15beb3e5b97f96f6bbecf4310fc
Максим Арзамасцев <kyhnya@yandex.ru>
2022-06-03 14:15:49 +0300

    staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,9 +139,9 @@ EMPTY_VALUE = '-пусто-'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
