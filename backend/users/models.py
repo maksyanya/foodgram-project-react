3ef58fb7b27b382deb43f5ade977d2b3307d886b
Максим Арзамасцев <kyhnya@yandex.ru>
2022-06-16 10:05:34 +0300

    enjoymeal

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,30 +9,30 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
 
-    username = models.CharField( 
-        db_index=True, 
-        max_length=150, 
-        unique=True, 
-        verbose_name='Уникальное имя', 
-        help_text='Введите уникальное имя пользователя') 
-    email = models.EmailField( 
-        db_index=True, 
-        unique=True, 
-        max_length=254, 
-        verbose_name='Электронная почта', 
-        help_text='Введите электронную почту пользователя') 
-    first_name = models.CharField( 
-        max_length=150, 
-        verbose_name='Имя', 
-        help_text='Введите имя пользователя') 
-    last_name = models.CharField( 
-        max_length=150, 
-        verbose_name='Фамилия', 
-        help_text='Введите фамилию пользователя') 
-    is_subscribed = models.BooleanField( 
-        default=False, 
-        verbose_name='Подписка на данного пользователя', 
-        help_text='Отметьте для подписки на данного пользователя') 
+    username = models.CharField(
+        db_index=True,
+        max_length=150,
+        unique=True,
+        verbose_name='Уникальное имя',
+        help_text='Введите уникальное имя пользователя')
+    email = models.EmailField(
+        db_index=True,
+        unique=True,
+        max_length=254,
+        verbose_name='Электронная почта',
+        help_text='Введите электронную почту пользователя')
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        help_text='Введите имя пользователя')
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        help_text='Введите фамилию пользователя')
+    is_subscribed = models.BooleanField(
+        default=False,
+        verbose_name='Подписка на данного пользователя',
+        help_text='Отметьте для подписки на данного пользователя')
 
     def __str__(self):
         """Represent the model by a string."""
